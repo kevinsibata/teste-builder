e1bc839db5949e5c203cfc5dffa575269f1fa6fb
kevin.sibata <kevin.sibata@gmail.com>
2023-09-23 15:02:02 -0300

    Ajuste de cores e criação de componente H1

--- builder-registry.ts
+++ builder-registry.ts
@@ -1,6 +1,7 @@
 "use client";
 import { Builder } from "@builder.io/react";
 import Counter from "./components/Counter/Counter";
+import Heading1 from "./components/Heading1/Heading1";
 
 Builder.registerComponent(Counter, {
   name: "Counter",
@@ -10,4 +11,11 @@ Builder.registerComponent(Counter, {
       type: "number",
     },
   ],
+});
+
+Builder.registerComponent(Heading1, {
+  name: "Heading 1",
+  inputs: [{ name: "title", type: "text" }],
+  image:
+    "https://tabler-icons.io/static/tabler-icons/icons-png/3d-cube-sphere-off.png",
 });
---
+++ components/Heading1/Heading1.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React, { useState } from "react";
+
+interface Heading1Props {
+  title?: string;
+}
+
+function Heading1({ title }: Heading1Props) {
+  return <h1>{title}</h1>;
+}
+
+export default Heading1;
